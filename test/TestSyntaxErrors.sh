3691d1cbfd418c98c7f03213601973154135d45d
SophieKoonin <sophie.koonin@gmail.com>
2015-07-08 15:20:11 +0100

    Separate bash file for batch testing syntax error messages

---
+++ test/TestSyntaxErrors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#compile all jdc test files
+
+for file in exceptions/*.jdc
+do
+	echo "Exception testing: compiling $file"
+	java -cp ../target/classes/ parser.JDCParser $file
+	echo "-------------------"
+done
+echo "Finished!"
