67f728b9b9314edbb2a301543aae32b33bc21a6d
SophieKoonin <sophie.koonin@gmail.com>
2015-07-09 14:51:09 +0100

    Test file for plain Java code in JDC files

---
+++ test/javacode/TestJavaCode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#compile all jdc test files
+
+for file in *.jdc
+do
+	filejava="${file%.*}"
+	filejava="$filejava.java"
+	echo "Compiling $file"
+	java -cp ../../target/classes/ parser.JDCParser $file
+	echo "-----"
+	diff -b -s $filejava ../expected/$filejava
+	echo "-------------------"
+done
+echo "Finished!"
