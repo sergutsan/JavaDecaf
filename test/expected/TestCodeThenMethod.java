ffef66d543a9c83bef4dfa098cf268f8c96e3cb1
SophieKoonin <sophie.koonin@gmail.com>
2015-07-09 15:03:11 +0100

    Create expected java classes for method test files

---
+++ test/expected/TestCodeThenMethod.java
@@ -0,0 +1,9 @@
+public class TestCodeThenMethod {
+    public static void main(String[] args) {
+        int i = 0;
+    }
+
+    public void pointlessMethod(){
+        String pointless = "I don't do anything";
+    }
+}
---
+++ test/expected/TestCodeThenMethod2.java
@@ -0,0 +1,11 @@
+public class TestCodeThenMethod2 {
+    public static void main(String[] args) {
+        sayHi();
+    }
+
+    public static void sayHi(){
+        System.out.println("Hi");
+    }
+
+
+}
