d689d675375f04aee24a63cf1d3b7d99eeb006d9
SophieKoonin <sophie.koonin@gmail.com>
2015-07-09 14:57:54 +0100

    Fix file path

--- test/syntaxerrors/TestSyntaxErrors.sh
+++ test/syntaxerrors/TestSyntaxErrors.sh
@@ -2,7 +2,7 @@
 
 #compile all jdc test files
 
-for file in exceptions/*.jdc
+for file in *.jdc
 do
 	echo "Exception testing: compiling $file"
 	java -cp ../../target/classes/ parser.JDCParser $file
