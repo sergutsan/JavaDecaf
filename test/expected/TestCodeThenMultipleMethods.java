24d723c53753a4bce1fb42f8caf59d0a9171cce9
SophieKoonin <sophie.koonin@gmail.com>
2015-07-13 13:27:38 +0100

    Expected outcome file for TestCodeThenMultipleMethods.jdc

---
+++ test/expected/TestCodeThenMultipleMethods.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TestCodeThenMultipleMethods {
+    private Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+        int i = 10;
+    }
+
+    private static int increment(int i) {
+        return i++;
+    }
+    private static int decrement(int i) {
+        return i--;
+    }
+}
