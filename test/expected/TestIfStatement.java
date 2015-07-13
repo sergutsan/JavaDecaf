136f6c246c88337cc9b70e99494e23eeecb726d3
SophieKoonin <sophie.koonin@gmail.com>
2015-07-13 17:03:56 +0100

    Further tests

---
+++ test/expected/TestIfStatement.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TestIfStatement {
+    private Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+        int i = 10;
+
+        if (i < 5) {
+            System.out.println("yes");
+        }
+    }
+
+}
