8a711ef1d292ddc683fc7c9ab2ead09a3e867afb
SophieKoonin <sophie.koonin@gmail.com>
2015-07-13 17:43:55 +0100

    tests

---
+++ test/expected/TestforLoop.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TestForLoop {
+    private Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        for (int i = 0; i<10; i++) {
+            System.out.println(i);
+        }
+    }
+}
