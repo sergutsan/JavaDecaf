3eb74bcb2f180b091d27cf50105350c5d43cfbe2
SophieKoonin <sophie.koonin@gmail.com>
2015-07-13 15:01:51 +0100

    Add a suggestion of missing quotation mark to LexicalError type of TokenMgrError

--- src/parser/TokenMgrError.java
+++ src/parser/TokenMgrError.java
@@ -109,7 +109,7 @@ public class TokenMgrError extends Error
           errorLine + ", column " +
           errorColumn + ".  Encountered: " +
           (EOFSeen ? "<EOF> " : ("\"" + addEscapes(String.valueOf(curChar)) + "\"") + " (" + (int)curChar + "), ") +
-          "after : \"" + addEscapes(errorAfter) + "\"");
+          "after : \"" + addEscapes(errorAfter) + "\" You may have forgotten a quotation mark around your String.");
   }
 
   /**
