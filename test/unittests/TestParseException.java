e35eeda86a8320cd2463322d4ab65cea9b45772a
SophieKoonin <sophie.koonin@gmail.com>
2015-07-14 13:24:39 +0100

    change to static references

--- test/unittests/TestParseException.java
+++ test/unittests/TestParseException.java
@@ -8,23 +8,17 @@ import static org.junit.Assert.*;
  * Tests for individual methods within parser.ParseException.
  */
 public class TestParseException {
-    private static ParseException ex;
-
-    @BeforeClass
-    public static void setUp(){
-        ex = new ParseException();
-    }
 
     @Test
     public void testIsReservedKeyword(){
         for (int i = 9; i < 59; i++){
-            assertTrue(ex.isReservedKeyword(i));
+            assertTrue(ParseException.isReservedKeyword(i));
         }
     }
 
     @Test
     public void testIsIdentifier(){
-        assertTrue(ex.isIdentifier(67));
+        assertTrue(ParseException.isIdentifier(67));
     }
 
 }
