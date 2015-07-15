cdeee71cace3ba69261e6e2f3fdd6141dddf8523
SophieKoonin <sophie.koonin@gmail.com>
2015-07-15 15:40:19 +0100

    Mismatched parens - check to see if rparen is first or second in expected list

--- src/parser/ParseException.java
+++ src/parser/ParseException.java
@@ -153,15 +153,17 @@ public class ParseException extends Exception {
       } else if ((isIdentifier(currentToken.kind)|| currentToken.image.equals("void") || isReservedKeyword(currentToken.kind)) && //identifier or "void" followed by reserved keyword
               isReservedKeyword(nextToken.kind)) {
           retval += "\"" + nextToken.image + "\" is a reserved keyword in Java and cannot be used as a method or variable name.";
-      } else if ((nextToken.kind == JDCParserConstants.IF || nextToken.kind == JDCParserConstants.FOR || nextToken.kind == JDCParserConstants.WHILE) && nextToken.next.kind != JDCParserConstants.RPAREN) {
-        retval += "You may have forgotten parentheses around the loop condition: e.g. if (x > y) { ... }" +
+      } else if ((nextToken.kind == JDCParserConstants.IF || nextToken.kind == JDCParserConstants.FOR || nextToken.kind == JDCParserConstants.WHILE)
+              && nextToken.next.kind != JDCParserConstants.RPAREN) {   //lookahead spots an error in loop declaration - nextToken is loop keyword
+        retval += "You may have forgotten parentheses around the loop condition: e.g. if (x > y) { println(x); }" +
                 "\n\'if\', \'for\' and \'while\' loop conditions should be in parentheses () and the loop body should be in curly braces { }.";
-      } else if ((currentToken.kind == JDCParserConstants.IF || currentToken.kind == JDCParserConstants.FOR || currentToken.kind == JDCParserConstants.WHILE) && nextToken.kind != JDCParserConstants.RPAREN) {
-          retval += "You may have forgotten parentheses around the loop condition: e.g. if (x > y) { ... }" +
+      } else if ((currentToken.kind == JDCParserConstants.IF || currentToken.kind == JDCParserConstants.FOR || currentToken.kind == JDCParserConstants.WHILE)
+              && nextToken.kind != JDCParserConstants.RPAREN) { //error with loop, no lookahead - currentToken is loop keyword
+          retval += "You may have forgotten parentheses around the loop condition: e.g. if (x > y) { println(x); }" +
                   "\n\'if\', \'for\' and \'while\' loop conditions should be in parentheses () and the loop body should be in curly braces { }.";
       } else if ((isIdentifier(currentToken.kind) || isPrimitive(currentToken.kind)) && nextToken.kind == JDCParserConstants.ASSIGN) {
           retval += "You may have forgotten to define a name for a variable: e.g. int myNum = 5; ";
-      } else if ((nextToken.kind == JDCParserConstants.RBRACKET || nextToken.kind == JDCParserConstants.RBRACE) && expectedTokenSequences[1][0] == JDCParserConstants.RPAREN) {
+      } else if ((nextToken.kind == JDCParserConstants.RBRACKET || nextToken.kind == JDCParserConstants.RBRACE) && (expectedTokenSequences[1][0] == JDCParserConstants.RPAREN || expectedTokenSequences[0][0] == JDCParserConstants.RPAREN)) {
           retval += "You may have used a brace { } or square bracket [ ] instead of a parenthesis ( ).";
       }
 
