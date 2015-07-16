893efdac4c96165741373176914fc072b2ffbd71
SophieKoonin <sophie.koonin@gmail.com>
2015-07-16 16:24:48 +0100

    Change return type of precompile to String to signal successful completion

--- src/parser/JDCCompiler.java
+++ src/parser/JDCCompiler.java
@@ -21,32 +21,40 @@ public class JDCCompiler {
             System.out.println("Usage: \"javadecaf filename\"");
         }
     }
-    public static void precompile(String filename) {
+
+    /**
+     * Call the JavaCC parser on the file from args[0] to convert the JavaDecaf code to true Java.
+     * @param inputFile the name of the file to be used as input
+     * @return the filename of the converted java file, null if something goes wrong
+     */
+    public static String precompile(String inputFile) {
         JDCParser parser;
         ASTCompilationUnit node;
         String className = "";
             try {
-                if (Character.isDigit(filename.charAt(0))) { throw new ParseException("Class names in Java cannot begin with a digit."+
+                if (Character.isDigit(inputFile.charAt(0))) { throw new ParseException("Class names in Java cannot begin with a digit."+
                         "Please rename the file.");}
-                parser = new JDCParser(new FileInputStream(filename));
-                int index = filename.indexOf("."); //get the index of the full stop for substring
-                className = filename.substring(0, index); //get the name of the class from the filename (before extension)
+                parser = new JDCParser(new FileInputStream(inputFile));
+                int index = inputFile.indexOf("."); //get the index of the full stop for substring
+                className = inputFile.substring(0, index); //get the name of the class from the filename (before extension)
                 node = parser.CompilationUnit();
                 PrintWriter ostr = new PrintWriter(new FileWriter(className+".java"));
-                node.process(ostr, filename);
+                node.process(ostr, className);
                 ostr.close();
                 System.out.println("OK");
+                return className + ".java"; //return the finished filename to signal successful compilation
             } catch (ParseException e) {
                 System.out.println("Errors during compilation: ");
                 System.out.println(e.getMessage());
             } catch (FileNotFoundException e) {
-                    System.out.println("File " + filename + " not found.");
+                    System.out.println("File " + inputFile + " not found.");
             } catch (IOException e) {
                 System.out.println("Error creating file " + className + ".java");
             } catch (TokenMgrError e) {
                 System.out.println(e.getMessage());
                 if (e.errorCode != TokenMgrError.LEXICAL_ERROR) e.printStackTrace(); //only print stack trace if error is not lexical (i.e. problem with compiler)
             }
+        return null;
     }
 
 }
