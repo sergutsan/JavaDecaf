5484d306abfbaaac5cbe32009936581094c48eab
SophieKoonin <sophie.koonin@gmail.com>
2015-07-16 16:04:13 +0100

    Revert

--- src/javacc/Rebuild.sh
+++ src/javacc/Rebuild.sh
@@ -2,5 +2,7 @@
 
 #compile jjt
 jjtree JavaDecaf.jjt
+#move the output jj file to the current folder
+mv ../parser/JavaDecaf.jj JavaDecaf.jj
 #compile jj
 javacc JavaDecaf.jj
--- src/parser/JDCCompiler.java
+++ src/parser/JDCCompiler.java
@@ -12,20 +12,7 @@ public class JDCCompiler {
     private JavaCompiler compiler = ToolProvider.getSystemJavaCompiler(); //get the local system java compiler
 
     public static void main(String[] args) {
-        JDCParser parser;
-        String className;
-        if (args.length == 1) {
-            int index = args[0].indexOf("."); //get the index of the full stop for substring
-            className = args[0].substring(0,index); //get the name of the class from the filename (before extension)
-            try {
-                parser = new JDCParser(new FileInputStream(args[0]));
-                parser.parse();
-            } catch (FileNotFoundException e) {
-                System.out.println("File " + args[0] + " not found.");
-            }
-        } else {
-            System.out.println("JavaDecaf Compiler:  Usage is \"javadecaf filename\"");
-        }
+
     }
 
 }
