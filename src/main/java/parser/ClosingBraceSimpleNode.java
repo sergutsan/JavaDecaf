7d1752480efde4247ca51742fbfabe16afb5ab83
SophieKoonin <sophie.koonin@gmail.com>
2015-07-22 13:40:01 +0100

    Calculate level of indentation and append correct number of spaces to closing brace.

--- src/main/java/parser/ClosingBraceSimpleNode.java
+++ src/main/java/parser/ClosingBraceSimpleNode.java
@@ -7,6 +7,8 @@ import java.io.PrintWriter;
  * @author Sophie Koonin
  */
 public class ClosingBraceSimpleNode extends SimpleNode {
+    private static final String INDENTATION_SPACES = "    ";
+
     public ClosingBraceSimpleNode(int i) {
         super(i);
     }
@@ -16,7 +18,17 @@ public class ClosingBraceSimpleNode extends SimpleNode {
 
     public void process(PrintWriter ostr) {
         super.process(ostr);
-        Token closingBrace = Token.newToken(73, "}");
+        int indentationLevel = 0;
+        Node node = this;
+        while(node.jjtGetParent() != null) {
+            indentationLevel++;
+            node = node.jjtGetParent(); //iterate through parents to find out how indented this should be
+        }
+        String indentation = "";
+        for (int i = 0; i<indentationLevel; i++){
+            indentation += INDENTATION_SPACES;
+        }
+        Token closingBrace = Token.newToken(73, indentation + "}");
         closingBrace.specialToken = new Token(0, "\n");
         print(closingBrace,ostr);
 
