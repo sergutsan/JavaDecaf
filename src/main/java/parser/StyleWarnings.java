747caa10d624bd6a0716b9da46979f31ea6393b8
SophieKoonin <sophie.koonin@gmail.com>
2015-07-22 10:36:08 +0100

    Create warnings class to identify style problems

---
+++ src/main/java/parser/StyleWarnings.java
@@ -0,0 +1,34 @@
+package main.java.parser;
+
+/**
+ * Class containing static methods for throwing out style warnings.
+ * @author Sophie Koonin
+ */
+public class StyleWarnings {
+    private static final int INDENTATION_SPACES = 4;
+
+    public static void checkIndentation(Token t, int indentationLevel) {
+        int indentationCount = 0;
+        Token countToken = t;
+        while (countToken != null) {
+            if (countToken.specialToken.image.equals(" ")) {
+                indentationCount++;
+            }
+            countToken = countToken.specialToken;
+        }
+        /* if the actual number of indented spaces doesn't match the indentation level multiplied by the number
+        of spaces to indent, add this warning to the warning list */
+
+        if (indentationCount != (indentationLevel * INDENTATION_SPACES)) {
+            String warning = "Warning - line " + t.endLine + ", column " + t.endColumn +
+                    ": contents of loop should be indented by four spaces. e.g.:" +
+                    "\nif (x == y) {" +
+                    "\n    println(x);"+
+                    "\n}";
+            JDCParser.addWarning(warning);
+        }
+
+
+    }
+
+}
