9358dd003aa3028b0dd89ecf9886904c20b3d180
SophieKoonin <sophie.koonin@gmail.com>
2015-07-22 12:57:18 +0100

    Create class ClosingBraceSimpleNode extending SimpleNode, which calls process() and prints an extra closing brace

---
+++ src/main/java/parser/ClosingBraceSimpleNode.java
@@ -0,0 +1,20 @@
+package main.java.parser;
+
+import java.io.PrintWriter;
+
+/**
+ * Subclass of SimpleNode which adds a closing brace to the end of anything printed.
+ * @author Sophie Koonin
+ */
+public class ClosingBraceSimpleNode extends SimpleNode {
+    public ClosingBraceSimpleNode(JDCParser p, int i) {
+        super(p, i);
+    }
+
+    public void process(PrintWriter ostr) {
+        super.process(ostr);
+        print(Token.newToken(73),ostr);
+
+    }
+
+}
