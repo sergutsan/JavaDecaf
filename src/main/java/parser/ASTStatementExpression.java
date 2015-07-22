dcfd889cbaa711a99b6d0acc0a304e09c387096c
SophieKoonin <sophie.koonin@gmail.com>
2015-07-22 11:15:22 +0100

    Create process

--- src/main/java/parser/ASTStatementExpression.java
+++ src/main/java/parser/ASTStatementExpression.java
@@ -2,6 +2,8 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=true,VISITOR=false,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 package main.java.parser;
 
+import java.io.PrintWriter;
+
 public
 class ASTStatementExpression extends SimpleNode {
   public ASTStatementExpression(int id) {
@@ -12,5 +14,12 @@ class ASTStatementExpression extends SimpleNode {
     super(p, id);
   }
 
+  public void process(PrintWriter ostr) {
+    /* if the parent node is a loop, check indentation of first token */
+    if (jjtGetParent().toString().equals("IfStatement")) {
+      StyleWarnings.checkIndentation(p, begin, 1);
+
+    }
+  }
 }
 /* JavaCC - OriginalChecksum=e873b876e7f466c17fe124d220aedcae (do not edit this line) */
