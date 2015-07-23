b0448376997f14a4b38fad87152182107ca0700e
SophieKoonin <sophie.koonin@gmail.com>
2015-07-23 10:24:40 +0100

    Fix package names

--- test/lexicalerrors/TestLexicalErrors.sh
+++ test/lexicalerrors/TestLexicalErrors.sh
@@ -5,7 +5,7 @@
 for file in *.jdc
 do
 	echo "Lexical error testing: compiling $file"
-	java -cp ../../target/classes/ parser.JavaDecafCompiler $file
+	java -cp ../../target/classes/ main.java.parser.JavaDecafCompiler $file
 	echo "-------------------"
 done
 echo "Finished!"
