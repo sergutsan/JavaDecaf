90283c18237d2160fb2d3c86a0ddf2535cd5e28e
SophieKoonin <sophie.koonin@gmail.com>
2015-07-24 18:03:07 +0100

    ?New test class

---
+++ test/expected/TestMethodWithSingleLineBreak.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TestMethodWithSingleLineBreak {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int i = 10;
+    }
+
+    private static void foo(){
+        System.out.println("foo");
+    }
+
+}
