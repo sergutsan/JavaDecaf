91c97ca9e9efcb5d44b04240bc374849af69b9e1
SophieKoonin <sophie.koonin@gmail.com>
2015-07-24 11:03:47 +0100

    Only main method declaration in DecafBlock

--- src/main/java/ast/ASTDecafBlock.java
+++ src/main/java/ast/ASTDecafBlock.java
@@ -19,18 +19,14 @@ public class ASTDecafBlock extends ClosingBraceSimpleNode {
         super(p, id);
     }
     /**
-     * Encapsulate a "loose" block of Decaf code with class and main method declarations.
+     * Encapsulate a "loose" block of Decaf code with main method declarations.
      * @param ostr - output stream writer defined in JDCParser
      */
   public void process (PrintWriter ostr) {
-      String classDec = "import java.util.Scanner;\n" +  //Assign the class/main method encapsulation code
-              "public class " + parser.getClassName() + " { \n" + ASTUtils.INDENTATION +
-              "private Scanner input = new Scanner(System.in);\n"  + ASTUtils.INDENTATION +//init Scanner for reading from stdin
-              "public static void main(String[] args) {";
-      ostr.print(classDec);
+      String mainDec = "\n" + ASTUtils.INDENTATION + "public static void main(String[] args) {";
+      ostr.print(mainDec);
 
       super.process(ostr);
-      ostr.print("\n}"); //print final closing brace of class
   }
 
 }
