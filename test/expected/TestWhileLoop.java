a752686872bbc880ede4c6df3a9276b5cece3d41
SophieKoonin <sophie.koonin@gmail.com>
2015-07-27 11:20:53 +0100

    New test files

---
+++ test/expected/TestWhileLoop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TestWhileLoop {
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = 5;
+        while(x == 10) {
+            System.out.println(x);
+        }
+    }
+}
