6a3909083fdfc33822c6da63dcd8e368ded6fef3
SophieKoonin <sophie.koonin@gmail.com>
2015-07-27 16:58:55 +0100

    check if a default case is present, if not, return a warning

--- src/main/java/ast/ASTSwitchStatement.java
+++ src/main/java/ast/ASTSwitchStatement.java
@@ -4,6 +4,7 @@ package main.java.ast;
 
 import main.java.parser.*;
 
+import javax.swing.text.Style;
 import java.io.PrintWriter;
 
 public
@@ -19,15 +20,19 @@ class ASTSwitchStatement extends ConditionalClosingBraceSimpleNode implements In
     public void process(PrintWriter ostr) {
         int breakCount = 0;
         int statementCount = 0;
+        boolean defaultPresent = false;
         for (int i = 0; i < jjtGetNumChildren(); i++){
             if (jjtGetChild(i) instanceof ASTBreakStatement) {
                 breakCount++;
             } else if (jjtGetChild(i) instanceof ASTStatementExpression) {
                 statementCount++;
+            } else if (jjtGetChild(i) instanceof ASTDefaultSwitchLabel) {
+                defaultPresent = true;
+                statementCount++;
             }
         }
         if (breakCount != statementCount) {
-            String warning = "Each case in a switch statement should be terminated with \"break;\", e.g.:"+
+            String warning = "Each case in a switch block should be terminated with \"break;\" to stop it continuing to execute, e.g.:"+
                     "\nswitch(number) {"+
                     "\n    case 0: "+
                     "\n        println(\"zero\");" +
@@ -39,6 +44,21 @@ class ASTSwitchStatement extends ConditionalClosingBraceSimpleNode implements In
                     "\n        break;" +
                     "\n}";
             StyleWarnings.generateWarning(begin, parser, warning);
+        }
+
+        if (!defaultPresent) {
+            String warning = "Switch blocks must end with a default case, which handles any values not covered by the cases, e.g.:"+
+                    "\nswitch(number) {"+
+                    "\n    case 0: "+
+                    "\n        println(\"zero\");" +
+                    "\n        break;" +
+                    "\n    case 1: "+
+                    "\n        println(\"one\");"+
+                    "\n        break;"+
+                    "\n    default:"+
+                    "\n        break;" +
+                    "\n}";
+            StyleWarnings.generateWarning(begin,parser,warning);
         }
         super.process(ostr);
     }
