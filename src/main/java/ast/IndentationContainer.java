072d9833a4d61c3b1d83149ef8851f6f8e077851
SophieKoonin <sophie.koonin@gmail.com>
2015-07-27 10:43:24 +0100

    Interface for nodes which contain indented contents, in order to simplify identification of nodes whose contents need to be checked for indentation

---
+++ src/main/java/ast/IndentationContainer.java
@@ -0,0 +1,7 @@
+package main.java.ast;
+
+/**
+ * Interface for nodes which contain indented contents, e.g. if loops, switch statements, method declarations.
+ */
+public interface IndentationContainer {
+}
