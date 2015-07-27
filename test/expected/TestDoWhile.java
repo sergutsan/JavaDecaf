978ca814d960a3d5270cb7fc43b16a0de2e20e20
SophieKoonin <sophie.koonin@gmail.com>
2015-07-27 18:25:19 +0100

    Expected test for do while

---
+++ test/expected/TestDoWhile.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TestDoWhile {
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        do {
+            System.out.println("Hello world!");
+        } while (true);
+    }
+}
