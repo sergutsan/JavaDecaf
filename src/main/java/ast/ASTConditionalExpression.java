c79156b02b9bb7c934c8e590216b6bdb0136ea9e
SophieKoonin <sophie.koonin@gmail.com>
2015-07-27 09:30:56 +0100

    Remove single OR/AND checking from jjt grammar and into ASTConditionalExpression

--- src/main/java/ast/ASTConditionalExpression.java
+++ src/main/java/ast/ASTConditionalExpression.java
@@ -8,13 +8,29 @@ import java.io.PrintWriter;
 
 public
 class ASTConditionalExpression extends SimpleNode {
-  public ASTConditionalExpression(int id) {
-    super(id);
-  }
+    public ASTConditionalExpression(int id) {
+        super(id);
+    }
 
-  public ASTConditionalExpression(JDCParser p, int id) {
-    super(p, id);
-    indent = false;
-  }
+    public ASTConditionalExpression(JDCParser p, int id) {
+        super(p, id);
+        indent = false;
+    }
+
+    public void process(PrintWriter ostr) {
+        Token t = begin;
+        while (t!= end) {
+            if (t.image.equals("&")) {
+                String warning = "\"You have used single \\'&\\' instead of double \\'&&\\'. Logical AND in Java is represented using \\'&&\\'.\"";
+                StyleWarnings.generateWarning(t, parser, warning);
+            }
+            if (t.image.equals("|")) {
+                String warning = "You have used single \\'|\\' instead of double \\'||\\'. Logical OR in Java is represented using \\'||\\'.\"";
+                StyleWarnings.generateWarning(t, parser, warning);
+            }
+            t = t.next;
+        }
+    }
+
 }
 /* JavaCC - OriginalChecksum=38ffaa9dd63c3f3e866095e4532ecd91 (do not edit this line) */
