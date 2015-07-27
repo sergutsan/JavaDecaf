4db0d8be9788e1002a29a5fac6b7250fc401d699
SophieKoonin <sophie.koonin@gmail.com>
2015-07-27 09:06:19 +0100

    Create expected testfile

---
+++ test/expected/TestIfElse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TestIfElse {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
