5b54a0da57255bed3a58ba07f795eef7b1beaf43
SophieKoonin <sophie.koonin@gmail.com>
2015-07-28 12:34:55 +0100

    Block extends ClosingBrace

--- src/main/java/ast/ASTBlock.java
+++ src/main/java/ast/ASTBlock.java
@@ -5,7 +5,7 @@ package main.java.ast;
 import main.java.parser.*;
 
 public
-class ASTBlock extends SimpleNode implements IndentationContainer {
+class ASTBlock extends ClosingBraceSimpleNode implements IndentationContainer {
   public ASTBlock(int id) {
     super(id);
   }
