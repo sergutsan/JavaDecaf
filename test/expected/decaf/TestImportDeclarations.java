e8319fdd1da9f6380d967da1013c7194b844eac7
SophieKoonin <sophie.koonin@gmail.com>
2015-07-28 15:09:35 +0100

    test of imports

---
+++ test/expected/decaf/TestImportDeclarations.java
@@ -0,0 +1,11 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class TestImportDeclarations {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Calendar cal = new GregorianCalendar();
+    }
+}
