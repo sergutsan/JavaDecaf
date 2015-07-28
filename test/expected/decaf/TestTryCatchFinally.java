70467e9cd77bc9d2d0c73b3c164172a185400f5f
SophieKoonin <sophie.koonin@gmail.com>
2015-07-28 12:54:56 +0100

    Fix name of test class

--- test/expected/decaf/TestTryCatchFinally.java
+++ test/expected/decaf/TestTryCatchFinally.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TestTryCatch {
+public class TestTryCatchFinally {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
