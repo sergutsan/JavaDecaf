2c1a229a7def7aa86463bf18b97b88bbb4807022
SophieKoonin <sophie.koonin@gmail.com>
2015-07-28 15:54:35 +0100

    Remove call to setIndentationLevel

--- src/main/java/ast/StatementVariableSimpleNode.java
+++ src/main/java/ast/StatementVariableSimpleNode.java
@@ -6,8 +6,7 @@ import main.java.parser.Token;
 import java.io.PrintWriter;
 
 /**
- * This class deals with all the substitutions and indentation in the blocks of methods,
- * loops etc.
+ * This class deals with all the substitutionsin the blocks of methods, loops etc.
  * @author Sophie Koonin
  */
 public class StatementVariableSimpleNode extends SimpleNode implements Indentable {
@@ -20,13 +19,12 @@ public class StatementVariableSimpleNode extends SimpleNode implements Indentabl
     }
 
     /**
-     *
+     * Check through tokens to see if any substitutions are needed, then call superclass method
+     * to process content.
      * @param ostr the output stream to print to.
      */
     public void process(PrintWriter ostr) {
-        setIndentationLevel();
-
-    /* if the parent node is a loop, check indentation of first token */
+        /* Iterate through tokens checking each one  */
         Token t = begin;
         String prevToken = "";
         while (t != end) {
