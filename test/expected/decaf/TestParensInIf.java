f77e88406b35f44e79ab16e9681da3da23c82a95
SophieKoonin <sophie.koonin@gmail.com>
2015-07-28 14:27:03 +0100

    New test file - nested parens

---
+++ test/expected/decaf/TestParensInIf.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TestParensInIf {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        if (isTrue) {
+            System.out.println("Hello world!");
+        }
+    }
+
+    private static boolean isTrue(){
+        return true;
+    }
+}
