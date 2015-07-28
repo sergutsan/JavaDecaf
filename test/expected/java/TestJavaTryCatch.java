33739c975039ad4d683bf754d4bf52173525a565
SophieKoonin <sophie.koonin@gmail.com>
2015-07-28 13:45:01 +0100

    Fix wrong method names

--- test/expected/java/TestJavaTryCatch.java
+++ test/expected/java/TestJavaTryCatch.java
@@ -2,7 +2,7 @@ public class TestJavaTryCatch {
 
     public static void main(String[] args) {
         try {
-            println("Hello world!");
+            System.out.println("Hello world!");
         } catch (RuntimeException ex) {
             ex.printStackTrace();
         }
