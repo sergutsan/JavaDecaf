634811b63773dc69c46516914713c0309a607c2d
SophieKoonin <sophie.koonin@gmail.com>
2015-07-29 13:21:29 +0100

    ForStatement doesn't need the behaviour of ConditionalSimpleNode, so don't extend it

--- src/main/java/ast/ASTForStatement.java
+++ src/main/java/ast/ASTForStatement.java
@@ -5,7 +5,7 @@ package main.java.ast;
 import main.java.parser.JDCParser;
 
 public
-class ASTForStatement extends ConditionalSimpleNode {
+class ASTForStatement extends SimpleNode {
   public ASTForStatement(int id) {
     super(id);
   }
