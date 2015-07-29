fe1cfb5aef8aa1c31abde1ad501b342efdcb06d1
SophieKoonin <sophie.koonin@gmail.com>
2015-07-29 10:29:41 +0100

    Tests

---
+++ test/expected/java/TestJavaComplexIf.java
@@ -0,0 +1,10 @@
+public class TestJavaComplexIf {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        if (x > 2 && y < 10) {
+            x += y;
+        }
+    }
+}
