6e243d0031685586f901a29d0af3113534e94013
SophieKoonin <sophie.koonin@gmail.com>
2015-07-29 14:53:40 +0100

    Test files

---
+++ test/expected/decaf/TestPostIncDec.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class TestTernary {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i++);
+        System.out.println(i--);
+    }
+}
