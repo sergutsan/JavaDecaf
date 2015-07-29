9da97d7565b5987f77fbbce6d2bb24edddc9ce94
SophieKoonin <sophie.koonin@gmail.com>
2015-07-29 14:49:29 +0100

    Remove parens

--- test/expected/decaf/TestTernary.java
+++ test/expected/decaf/TestTernary.java
@@ -7,6 +7,6 @@ public class TestTernary {
     public static void main(String[] args) {
         int x = 10;
         int y = 20;
-        int z = (x > y) ? x : y;
+        int z = x > y ? x : y;
     }
 }
