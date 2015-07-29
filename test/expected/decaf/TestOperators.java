27be5f82abc18fa28b9fa45c3137efd9f1b585c9
SophieKoonin <sophie.koonin@gmail.com>
2015-07-29 15:56:37 +0100

    Tests

---
+++ test/expected/decaf/TestOperators.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class TestOperators {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 5;
+        int k = i + j;
+        int l = i - j;
+        int m = i * j;
+        int n = i / j;
+        int o = i % j;
+        int p = i < j ? i : j;
+        int q = i > j ? i : j;
+        int r = i <= j ? i : j;
+        int s = i <= j ? i : j;
+    }
+}
