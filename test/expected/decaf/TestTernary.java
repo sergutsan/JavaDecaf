53ad5c47f81360550a3eb21c2daeb6f453b9874c
SophieKoonin <sophie.koonin@gmail.com>
2015-07-29 14:46:55 +0100

    Tests

---
+++ test/expected/decaf/TestTernary.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class TestTernary {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = (x > y) ? x : y;
+    }
+}
