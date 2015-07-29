ed65bfa47b2531cc2bc30504c7c8b2cd0a5412e5
SophieKoonin <sophie.koonin@gmail.com>
2015-07-29 16:29:23 +0100

    add more to tests

--- test/expected/decaf/TestOperators.java
+++ test/expected/decaf/TestOperators.java
@@ -16,5 +16,7 @@ public class TestOperators {
         int q = i > j ? i : j;
         int r = i <= j ? i : j;
         int s = i <= j ? i : j;
+        int t = i == j? i : j;
+        int u = i != j? i : j;
     }
 }
