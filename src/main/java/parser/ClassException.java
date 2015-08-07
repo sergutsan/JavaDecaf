01bfd1bf3588a3903b60ea8393dcc3ba04112ded
SophieKoonin <sophie.koonin@gmail.com>
2015-08-07 12:49:09 +0100

    Create ClassNameException

---
+++ src/main/java/parser/ClassException.java
@@ -0,0 +1,26 @@
+package main.java.parser;
+
+/**
+ * Exception thrown when classname is not acceptable according to Java convention.
+ */
+public class ClassNameException extends ParseException {
+    protected static final String EOL = System.getProperty("line.separator", "\n");
+
+    public ClassNameException(Token t){
+        super(initMessage(t));
+    }
+
+    /**
+     * Initialise the message for this exception with the name of the class
+     * @param t the token of the classname
+     * @return the error message
+     */
+    public static String initMessage(Token t) {
+        String message = "Error: encountered class name " + t.image + " at line " + t.beginLine + ", column "+ t.beginColumn +
+                ". Class names should begin with a capital letter, and be written in CamelCase. They should also be nouns. e.g.:" + EOL +
+                "class Person" + EOL + "class FractionCalculator" + EOL + "class FlyingAnimal";
+        return message;
+
+    }
+
+}
