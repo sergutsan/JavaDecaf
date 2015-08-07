d1dfdcf27471d8ed36144f623a7532c97ee4ef6b
SophieKoonin <sophie.koonin@gmail.com>
2015-08-07 12:07:49 +0100

    Change WhileAfterDo last token to final semicolon. rebuild

--- src/main/java/parser/JDCParser.java
+++ src/main/java/parser/JDCParser.java
@@ -3332,7 +3332,7 @@ public class JDCParser/*@bgen(jjtree)*/implements JDCParserTreeConstants, JDCPar
       jj_consume_token(RPAREN);
   jjtree.closeNodeScope(jjtn001, true);
   jjtc001 = false;
-    setFirstLastToken(jjtn001, t, getToken(0));
+    setFirstLastToken(jjtn001, t, getToken(1));
     } catch (Throwable jjte001) {
   if (jjtc001) {
     jjtree.clearNodeScope(jjtn001);
