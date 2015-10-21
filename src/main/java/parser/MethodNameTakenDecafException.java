01e8ee9a37fab1ce681b5f29ffd12091ac86242e
Sergio Gutierrez-Santos "Sergut <sergut@gmail>
2015-10-21 21:37:50 +0100

    Java Decaf method's names cannot be used by students either.
    Modified method accordingly.

---
+++ src/main/java/parser/MethodNameTakenDecafException.java
@@ -0,0 +1,26 @@
+package parser;
+
+/**
+ * Exception thrown when method name is not acceptable according to Java convention.
+ */
+public class MethodNameTakenDecafException extends ParseException {
+    protected static final String EOL = System.getProperty("line.separator", "\n");
+
+    public MethodNameTakenDecafException(Token t){
+        super(initMessage(t));
+    }
+
+    /**
+     * Initialise the message for this exception with the name of the class
+     * @param t the token of the classname
+     * @return the error message
+     */
+    public static String initMessage(Token t) {
+        String message = "Error: encountered method name " + t.image + "() at line " + t.beginLine + ", column "+ t.beginColumn +
+		". The following method names are reserved by Java Decaf and cannot be used: " + EOL +
+		"print, println, readDouble, readInt, readLine." + EOL; 
+        return message;
+
+    }
+
+}
